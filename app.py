6725764794869a1e7474a640dfd48f0e92b0d655
Priyanshi Chauhan <PRIYANSHI.3816@GMAIL.COM>
2026-09-07 12:41:59 +0530

    new change

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, jsonify
 
-from database.database import create_tables
+from database.database import  create_tables
 from routes.auth import auth_bp
 from routes.user import user_bp
 from routes.learning import learning_bp
 from routes.assessment import assessment_bp
 
-
 app = Flask(__name__)
 
-
 create_tables()
-
 
 @app.route("/")
 def home():
@@ -20,12 +17,10 @@ def home():
         "status": "success"
     })
 
-
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(user_bp, url_prefix="/user")
 app.register_blueprint(learning_bp, url_prefix="/learning")
 app.register_blueprint(assessment_bp, url_prefix="/assessment")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
